3f58856eb8be22b0dfc530b48ae85c2521ad34b1
Minh Hưng <124641608+mhung2026@users.noreply.github.com>
2023-12-28 22:14:51 +0700

    Update

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 function blowCandles() {
     var flameElements = document.querySelectorAll('.flame, .flame1, .flame2');
-    // var showButton = document.querySelectorAll(".showButton")
+    var showButton = document.querySelectorAll(".showButton")
 
     flameElements.forEach(function (flameElement) {
         flameElement.style.opacity = '0';
     });
-    // showButton.forEach(function (showButton) {
-    //     showButton.style.opacity = '100';
-    // });
+    showButton.forEach(function (showButton) {
+        showButton.style.opacity = '100';
+    });
 }
 // document.addEventListener('DOMContentLoaded', function () {
 //     var showImageButton = document.getElementById('showImageButton');
