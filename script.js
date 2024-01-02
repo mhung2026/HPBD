3f62e4cfbe334f60782dbac6a2558d2b494651df
Minh Hưng <124641608+mhung2026@users.noreply.github.com>
2024-01-02 20:56:42 +0700

    Summary

--- script.js
+++ script.js
@@ -1,30 +1,18 @@
 function blowCandles() {
-    var flameElements = document.querySelectorAll('.flame');
     var audio = document.getElementById("HPBDAudio");
     audio.play();
-    audio.volume = 0.1;
+    audio.volume = 0;
+
+    var flameElements = document.querySelectorAll('.image');
+    var buttonElements = document.getElementById('showImageButton');
     flameElements.forEach(function (flameElement) {
-        flameElement.style.opacity = '0';
+        flameElement.style.display = 'none';
     });
+    // buttonElements.style.display = 'none';   
+    document.getElementById('gift').classList.toggle('hidden');
 }
 document.addEventListener('DOMContentLoaded', function () {
-    var showImageButton = document.getElementById('showImageButton');
-    var imageContainer = document.getElementById('imageContainer');
-    setTimeout(function () {
-        showImageButton.addEventListener('click', function () {
-            imageContainer.style.display = 'block';
-            var imgElement = document.createElement('img');
-            imgElement.src = 'https://github.com/mhung2026/test/blob/main/gift.png?raw=true';
-            imgElement.alt = 'Image';
-            imgElement.style.width = '60%';
-            imgElement.style.height = '60%';
-            imageContainer.innerHTML = '';
-            imageContainer.appendChild(imgElement);
-        });
-    }, 0);
-});
-document.addEventListener('DOMContentLoaded', function () {
-    var openWebsiteBtn = document.getElementById('openWebsiteBtn');
+    var openWebsiteBtn = document.getElementById('gift');
     openWebsiteBtn.addEventListener('click', function () {
         window.open('https://www.instagram.com/hung.2026', '_blank');
     });
